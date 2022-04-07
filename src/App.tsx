24c3057ae6b64c51916447ceacbd97f499e8b13b
toshiyuki-sato <toshiykst@gmail.com>
2022-04-07 11:44:03 +0900

    Create react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App: React.VFC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">React sentry sample</header>
+    </div>
+  );
+};
+
+export default App;
