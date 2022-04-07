f674bd2dd47ad475cd4452162ae4561a182aa663
toshiyuki-sato <toshiykst@gmail.com>
2022-04-07 20:16:00 +0900

    Add sentry init

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
+import "./index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 root.render(
   <React.StrictMode>
